d21c934bc4b5914c1da1e670513017820a2f1381
Tara Mahoney <tara.mahoney@me.com>
2015-04-22 15:02:23 -0400

    Adding authorization to make sure users are logged in to be able to post reviews & edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   private   
    helper_method :current_user #this turns the current_user method into a helper method that will be available in the views
+    
     def current_user
       #check if the user that sent this request has a session
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,7 +15,8 @@ class ApplicationController < ActionController::Base
 
     def ensure_logged_in
       unless current_user
-        redirect_to login_path
+        flash[:alert] = "Please log in to perform this action!"
+        redirect_to new_session_path
       end
     end    
 end
